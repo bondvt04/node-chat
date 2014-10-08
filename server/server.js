7b6f17d42757323e1f6240316f28e2c8b9d5bb05
Anatoliy Bondar <a.bondar@mobidev.biz>
2014-10-08 15:05:41 +0300

    server + client

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
-exports.start = function() {
-    var http = require("http");
-    var url = require("url");
-    
-    function onRequest(request, response) {
-        
-        var pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + " received.");
-        
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write("Hello World");
-        response.end();
-    }
-    http.createServer(onRequest).listen(8888);
-};
+var app = require('http').createServer();
+var io = require('socket.io').listen(app);
+var fs = require('fs');
 
+app.listen(7777);
+var serverName = '<b style="color:blue">SERVER</b>';
 
+io.sockets.on('connection', function(socket) {
+    console.log('new user connected...');
+
+    socket.on('addme', function(username) {
+        socket.username = username;
+        socket.emit('chat', serverName, 'You have connected');
+        socket.broadcast.emit('chat', serverName, username + ' is on deck :)');
+    });
+
+    socket.on('sendchat', function(data) {
+        socket.emit('chat', socket.username, data);
+    });
+
+    socket.on('disconnect', function() {
+        io.sockets.emit('chat', serverName, socket.username + ' has left the building...');
+    });
+});
+
+console.log('start server...');
+//file:///Users/anatoliybondar/www/node-chat/client/index.html
