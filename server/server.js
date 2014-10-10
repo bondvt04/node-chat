6963a8277bb79f664f497046f603272075f4c94f
Anatoliy Bondar <a.bondar@mobidev.biz>
2014-10-10 12:36:41 +0300

    colors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 var fs = require('fs');
+var colors = require('colors');
 
 var app = require('https').createServer({
     key : fs.readFileSync(__dirname+'/../ssl/site.key').toString(),
     cert : fs.readFileSync(__dirname+'/../ssl/final.crt').toString()
 }, function(request, response) {
-    console.log();
     fs.readFile(__dirname+'/../client/index.html', function(err, data) {
         if(err) {
             console.log(__diname);
@@ -21,37 +21,53 @@ var app = require('https').createServer({
 
         response.end(data);
     });
-}).listen("7777", "192.168.0.141", function(){
-    console.log("Server up and running...");
-});
+})
 
-
-var io = require('socket.io').listen(app);
-io.set('origins', '*:*');
-
-var serverName = '<b style="color:blue">SERVER</b>';
-
-io.sockets.on('connection', function(socket) {
-    console.log('new user connected...');
-
-    socket.on('addme', function(username) {
-        console.log("addme", username);
-
-        socket.username = username;
-        socket.emit('chat', serverName, 'You have connected');
-        socket.broadcast.emit('chat', serverName, username + ' is on deck :)');
+try {
+    app.listen("7777", "192.168.0.141", function(){
+        console.log("Server up and running...".rainbow.bold.underline);
+        //console.log("Server up and running...");
     });
+} catch(e) {
+    console.log("### Error");
+}
 
-    socket.on('sendchat', function(data) {
-        console.log("sendchat", data);
+//try {
+//    app.listen(443);
+//} catch(e) {
+//    console.log("### Error");
+//}
 
-        io.sockets.emit('chat', socket.username, data);
-    });
+if(true) {
+    try {
+        var io = require('socket.io').listen(app);
+        io.set('origins', '*:*');
 
-    socket.on('disconnect', function() {
-        io.sockets.emit('chat', serverName, socket.username + ' has left the building...');
-    });
-});
+        var serverName = '<b style="color:blue">SERVER</b>';
 
-//console.log('start server...');
-//file:///Users/anatoliybondar/www/node-chat/client/index.html
+        io.sockets.on('connection', function(socket) {
+            console.log('new user connected...');
+
+            socket.on('addme', function(username) {
+                console.log("addme", username);
+
+                socket.username = username;
+                socket.emit('chat', serverName, 'You have connected');
+                socket.broadcast.emit('chat', serverName, username + ' is on deck :)');
+            });
+
+            socket.on('sendchat', function(data) {
+                console.log("sendchat", data);
+
+                io.sockets.emit('chat', socket.username, data);
+            });
+
+            socket.on('disconnect', function() {
+                io.sockets.emit('chat', serverName, socket.username + ' has left the building...');
+            });
+        });
+    } catch(e) {
+        console.log("### Error");
+    }
+}
+
